6374c6cc51b6c07b832467bfec922d0fae3b14e7
HFeeney <haydenbfeeney@gmail.com>
2022-04-04 15:55:30 -0700

    final robot, constants, delivery command clean

--- src/main/java/frc/robot/commands/DeliveryCommand.java
+++ src/main/java/frc/robot/commands/DeliveryCommand.java
@@ -9,7 +9,6 @@ import frc.robot.Constants;
 import frc.robot.subsystems.Delivery;
 
 public class DeliveryCommand extends CommandBase {
-  private boolean photoTurnedOn;
   private Delivery delivery;
 
   /**
@@ -18,24 +17,17 @@ public class DeliveryCommand extends CommandBase {
    */
   public DeliveryCommand(Delivery delivery) {
     addRequirements(delivery);
-    photoTurnedOn = false;
     this.delivery = delivery;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    photoTurnedOn = false;
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     delivery.runMotor(Constants.Delivery.INDEXING_SPEED);
-    
-    if(delivery.getSensor2()){
-      photoTurnedOn = true;
-    }
   }
 
   // Called once the command ends or is interrupted.
@@ -47,9 +39,6 @@ public class DeliveryCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    // if(photoTurnedOn && !delivery.getSensor2()){
-    //   return true;
-    // }
     return false;
   }
 }
